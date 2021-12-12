c9e6b6ce1839822184908878438b4abae4a4e9c6
Brad <bradleyglaurie@gmail.com>
2021-12-12 16:05:42 +0000

    Fixed isolated.sh permission issue

--- isolated.sh
+++ isolated.sh
@@ -1,6 +1,6 @@
 # https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/6/html/virtualization_host_configuration_and_guest_installation_guide/app_macvtap
 sudo wget 'https://raw.githubusercontent.com/yobson1/vfio/main/config/isolated.xml' -O /tmp/isolated.xml
 virsh net-define /tmp/isolated.xml
-virsh net-autostart isolated
+sudo virsh net-autostart isolated
 sudo virsh net-start isolated
 sudo rm /tmp/isolated.xml
